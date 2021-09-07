5750f3772baec5a4cc4ef606fb3d8f716a158070
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-09-07 08:44:37 +0200

    Add logLevel to config

---
+++ src/config/config.test.js
@@ -0,0 +1,1 @@
+// TODO Create tests, but first move jest stuff from __TESTS__ to root
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,18 @@
-function initConfig(config) {
+import { logLevels } from '../logging'
+
+export function initConfig(config) {
   if (!config) config = {}
+
   if (!config.resourceNames || config.resourceNames.length === 0)
     config.resourceNames = ['articles']
-}
 
-export default { initConfig }
+  if (
+    !config.logLevel ||
+    config.logLevel.length === 0 ||
+    !Object.keys(logLevels).includes(config.logLevel.toUpperCase())
+  ) {
+    config.logLevel = logLevels.INFO
+  } else config.logLevel = config.logLevel.toUpperCase()
+
+  return config
+}
