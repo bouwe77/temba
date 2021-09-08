eb7ae0131f0fb0031e968cfb518b4c5be65b2979
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-09-08 10:11:06 +0200

    Added dotenv and MongoDB connection string

--- __tests__/app/server.js
+++ __tests__/app/server.js
@@ -1,11 +1,14 @@
+import 'dotenv/config'
 import { create } from '../../dist/server.js'
 
 const config = {
   resourceNames: ['movies'],
+  connectionString: process.env.MONGO_URL,
 }
+
 const server = create(config)
 
-const port = 3888
+const port = process.env.PORT || 3000
 server.listen(port, () => {
   console.log(`Temba is running on port ${port}`)
 })
