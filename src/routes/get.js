f55ef864911abe7028e2d46292ff24cada6d53f2
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-01-16 15:36:35 +0100

    Simplified setting the Cache-control header

--- src/routes/get.js
+++ src/routes/get.js
@@ -3,24 +3,23 @@ function createGetRoutes(queries, cacheControl) {
     try {
       const { resourceName, id } = req.requestInfo
 
+      res.set('Cache-control', cacheControl)
+
       if (id) {
         const item = await queries.getById(resourceName, id)
 
         if (!item) {
           res.status(404)
-          res.set('Cache-control', cacheControl)
           return res.send()
         }
 
         res.status(200)
-        res.set('Cache-control', cacheControl)
         res.json(item)
         return res.send()
       }
 
       const items = await queries.getAll(resourceName)
       res.status(200)
-      res.set('Cache-control', cacheControl)
       res.json(items)
       return res.send()
     } catch (error) {
