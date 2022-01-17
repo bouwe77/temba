75725bd12cc72457957bd7ac299f14e768629d79
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-01-17 21:08:30 +0100

    Reordering stuff

--- __tests__/e2e/app/server.js
+++ __tests__/e2e/app/server.js
@@ -1,9 +1,10 @@
 import 'dotenv/config'
 import { create } from '../../../dist/server.js'
 
+const port = process.env.PORT || 3000
+
 const server = create()
 
-const port = process.env.PORT || 3000
 server.listen(port, () => {
   console.log(`Temba is running on port ${port}`)
 })
