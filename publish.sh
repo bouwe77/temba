39bae3eb995b8a7913c4eae49a12580f40694f1f
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-07-17 22:48:24 +0200

    chore: add publish script

---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ]
+then
+  echo ""
+  echo "============================="
+  echo " I N V A L I D   O P T I O N"
+  echo "============================="
+  echo ""
+  echo "Usage: ./publish.sh [major|minor|patch]"
+  echo "Example: ./publish.sh patch"
+  echo ""
+  exit 1
+fi
+
+if [ -z "$(git status --porcelain)" ]; then 
+    npm version $1
+    npm run build
+    npm publish ./dist
+else 
+  echo "Error: Commit all changes before publishing"
+  exit 1
+fi
+
