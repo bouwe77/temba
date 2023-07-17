23aebad6e3749d329fc920d28ac479b9dd5a310d
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-07-17 23:39:36 +0200

    chore: skip git tag for now

--- publish.sh
+++ publish.sh
@@ -18,7 +18,7 @@ if [ -z "$(git status --porcelain)" ]; then
     npm run build
     npm publish ./dist
 
-    open https://github.com/octo-org/octo-repo/releases/new?tag=$version
+    open https://github.com/bouwe77/temba/releases/new?tag=$version
 
     # git tag $version -m "$version"
     # git push origin $version
