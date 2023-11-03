c5a542634de3d938aa45b5bba446543994de8c18
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-11-03 10:31:23 +0100

    tests: explain/document OPTIONS behavior

--- test/integration/options-method.test.ts
+++ test/integration/options-method.test.ts
@@ -1,16 +1,38 @@
 import request from 'supertest'
 import createServer from './createServer'
+import express from 'express'
 
 /*
   Tests for OPTIONS requests
 */
 
-// This Temba server is created with the default configuration, i.e. no config object is supplied.
 const tembaServer = createServer()
 
-const resource = '/cars/'
+const customRouter = express.Router()
+customRouter.get('/hello', async (_, res) => {
+  return res.send('Hello, World!')
+})
+
+// The bottom line of Express' behavior for OPTIONS requests
+// is that it always returns the same methods, unless you implement
+// your own OPTIONS handler.
+//
+// So for Temba this means the OPTIONS request is not "supported",
+// looking at allowed methods. For CORS purposes, Express' default
+// seems sufficient though.
 
 test('OPTIONS', async () => {
-  const optionsResponse = await request(tembaServer).options(resource)
-  expect(optionsResponse.status).toBe(204)
+  const optionsRootResponse = await request(tembaServer).options('/')
+  expect(optionsRootResponse.status).toBe(204)
+  expect(optionsRootResponse.header['access-control-allow-methods']).toBe(
+    'GET,HEAD,PUT,PATCH,POST,DELETE',
+  )
+
+  // Even though we only have a GET route for /hello, the OPTIONS response
+  // returns all methods that exists, because that's Express' default behavior.
+  const optionsHelloResponse = await request(tembaServer).options('/hello')
+  expect(optionsHelloResponse.status).toBe(204)
+  expect(optionsHelloResponse.header['access-control-allow-methods']).toBe(
+    'GET,HEAD,PUT,PATCH,POST,DELETE',
+  )
 })
