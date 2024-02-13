6540e401a2d016d430ac583bdf7b7f62791e5fbc
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2024-02-13 09:09:53 +0100

    add strictness

--- src/delay/delayMiddleware.ts
+++ src/delay/delayMiddleware.ts
@@ -1,7 +1,13 @@
-import pause from 'connect-pause'
+import { NextFunction, Request, Response } from 'express'
 
-function createDelayMiddleware(delay) {
-  return function (req, res, next) {
+function pause(delay: number) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    setTimeout(next, delay)
+  }
+}
+
+function createDelayMiddleware(delay: number) {
+  return function (req: Request, res: Response, next: NextFunction) {
     console.log('Start delay...')
     pause(delay)(req, res, next)
     console.log('Delay finished!')
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+Hier was ik gebleven:
+strictness in tsconfig toegevoegd, dus nu tsc uitvoeren
+en dan de errors oplossen
+
 import express, { json } from 'express'
 import morgan from 'morgan'
 import {
