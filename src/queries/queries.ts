620e3ab285f84b72ae48f4a162414c92083c3980
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2024-02-23 21:12:57 +0100

    feat: queries to file storage using lowdb

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,7 +1,11 @@
+import createFileQueries from './file'
 import inMemoryQueries from './in-memory'
 import createMongoQueries from './mongo'
 
 function createQueries(connectionString) {
+  // TODO figure out config later
+  return createFileQueries()
+
   if (!connectionString) {
     return inMemoryQueries
   }
