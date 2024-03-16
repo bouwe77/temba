5297cf64bcfdc051e6cd248b38ae143f0ccf1770
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2024-03-16 21:22:54 +0100

    fix: explicitely say Express is never undefined, because it is

--- test/integration/createServer.ts
+++ test/integration/createServer.ts
@@ -1,7 +1,8 @@
 import type { UserConfig } from '../../src/config'
 import { create } from '../../src/index'
 
-const createServer = (config?: UserConfig) =>
-  create({ ...(config || ({} as UserConfig)), isTesting: true }).Express
-
+const createServer = (config?: UserConfig) => {
+  const server = create({ ...(config || ({} as UserConfig)), isTesting: true })
+  return server.Express!
+}
 export default createServer
