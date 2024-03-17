8da7c74f8a7f673158c7c9873aeed22d103b0e7f
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2024-03-17 23:45:27 +0100

    feat: JSON file (and in-memory) storage using lowdb

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,11 +1,14 @@
+import createFileQueries from './file'
 import { inMemoryQueries } from './in-memory'
 import { createMongoQueries } from './mongo'
 
 export const createQueries = (connectionString: string | null) => {
-  if (!connectionString) {
-    return inMemoryQueries
-  }
+  return createFileQueries()
 
-  const mongoQueries = createMongoQueries(connectionString)
-  return mongoQueries
+  // if (!connectionString) {
+  //   return inMemoryQueries
+  // }
+
+  // const mongoQueries = createMongoQueries(connectionString)
+  // return mongoQueries
 }
