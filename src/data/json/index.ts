95f24494d178f95a513d38e183384cd450c9cd7e
Bouwe <bouwe@webinargeek.com>
2024-05-08 22:27:48 +0200

    feat: async lowdb

--- src/data/json/index.ts
+++ src/data/json/index.ts
@@ -1,24 +1,23 @@
 import type { Item, ItemWithoutId, Queries } from '../types'
-import { MemorySync, LowSync, type SyncAdapter } from 'lowdb'
-import { JSONFileSync } from 'lowdb/node'
+import { Low, type Adapter, Memory } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
 import type { PathLike } from 'node:fs'
 
 //TODO
-// * Can defaultData be the initialData provided through the config?
-// * Error handling for accessing the file
-// * JSON meuk async maken (zie https://github.com/typicode/lowdb/blob/main/src/presets/node.ts)
+// - [ ] Error handling for accessing the file
+// - [ ] JSON meuk async maken (zie https://github.com/typicode/lowdb/blob/main/src/presets/node.ts)
 
-const getInMemoryDb = <Data>(defaultData: Data): LowSync<Data> => {
-  return getJsonDb(new MemorySync<Data>(), defaultData)
+const getInMemoryDb = <Data>(defaultData: Data): Promise<Low<Data>> => {
+  return getJsonDb(new Memory<Data>(), defaultData)
 }
 
-const getFileDb = <Data>(filename: PathLike, defaultData: Data): LowSync<Data> => {
-  return getJsonDb(new JSONFileSync<Data>(filename), defaultData)
+const getFileDb = async <Data>(filename: PathLike, defaultData: Data): Promise<Low<Data>> => {
+  return await getJsonDb(new JSONFile<Data>(filename), defaultData)
 }
 
-const getJsonDb = <Data>(adapter: SyncAdapter<Data>, defaultData: Data): LowSync<Data> => {
-  const db = new LowSync<Data>(adapter, defaultData)
-  db.read()
+const getJsonDb = async <Data>(adapter: Adapter<Data>, defaultData: Data): Promise<Low<Data>> => {
+  const db = new Low<Data>(adapter, defaultData)
+  await db.read()
   return db
 }
 
@@ -27,70 +26,61 @@ type JsonConfig = {
 }
 
 export default function createJsonQueries({ filename }: JsonConfig) {
-  type Data = { [key: string]: Item[] }
-  const defaultData: Data = {}
+  const defaultData: { [key: string]: Item[] } = {}
 
-  const db = filename ? getFileDb(filename, defaultData) : getInMemoryDb(defaultData)
-
-  function getAll(resource: string) {
-    const data = db.data[resource] || []
-    return new Promise<Item[]>((resolve) => {
-      resolve(data)
-    })
+  async function getDb() {
+    const db = filename ? await getFileDb(filename, defaultData) : await getInMemoryDb(defaultData)
+    return db
   }
 
-  function getById(resource: string, id: string) {
+  async function getAll(resource: string) {
+    const db = await getDb()
     const data = db.data[resource] || []
-    const thingy = (data || []).find((x) => x.id === id) || null
-    return new Promise<Item | null>((resolve) => {
-      resolve(thingy)
-    })
+    return data
   }
 
-  function create(resource: string, item: ItemWithoutId) {
+  async function getById(resource: string, id: string) {
+    const db = await getDb()
+    const data = db.data[resource] || []
+    return (data || []).find((x) => x.id === id) || null
+  }
+
+  async function create(resource: string, item: ItemWithoutId) {
+    const db = await getDb()
     const itemWithId = { ...item, id: String(new Date().getTime()) } satisfies Item
-    db.update((data) => {
+    await db.update((data) => {
       data[resource] = [...(data[resource] || []), itemWithId]
     })
 
-    return new Promise<Item>((resolve) => {
-      resolve(itemWithId)
-    })
+    return itemWithId
   }
 
-  function update(resource: string, item: Item) {
+  async function update(resource: string, item: Item) {
     const updatedItem = { ...item } satisfies Item
 
-    db.update((data) => {
+    const db = await getDb()
+    await db.update((data) => {
       data[resource] = [...(data[resource] || []).filter((r) => r.id !== item.id), updatedItem]
     })
 
-    return new Promise<Item>((resolve) => {
-      resolve(updatedItem)
-    })
+    return updatedItem
   }
 
-  function replace(resource: string, item: Item) {
+  async function replace(resource: string, item: Item) {
     return update(resource, item)
   }
 
-  function deleteById(resource: string, id: string) {
-    db.update((data) => {
+  async function deleteById(resource: string, id: string) {
+    const db = await getDb()
+    await db.update((data) => {
       data[resource] = [...(data[resource] || []).filter((r) => r.id !== id)]
-    })
-
-    return new Promise<void>((resolve) => {
-      resolve()
     })
   }
 
-  function deleteAll(resource: string) {
-    db.update((data) => {
+  async function deleteAll(resource: string) {
+    const db = await getDb()
+    await db.update((data) => {
       data[resource] = []
-    })
-
-    return new Promise<void>((resolve) => {
-      resolve()
     })
   }
 
