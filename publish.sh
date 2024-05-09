4a9ef15ce5f61e11de97d1ba8f1dbc45e2cefff1
Bouwe <bouwe@webinargeek.com>
2024-05-09 22:14:52 +0200

    chore

--- publish.sh
+++ publish.sh
@@ -19,10 +19,6 @@ if [ -z "$(git status --porcelain)" ]; then
     npm publish ./dist/src
 
     open "https://github.com/bouwe77/temba/releases/new?tag=$version&title=$version&prerelease=1"
-
-    # git tag $version -m "$version"
-    # git push origin $version
-    # git push origin --tags
 else 
   echo "Error: Commit all changes before publishing"
   exit 1
