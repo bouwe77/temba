993b9b9860a5896d1c8f67fece088c8be80bee8c
Bouwe <bouwe@webinargeek.com>
2024-05-23 21:41:09 +0200

    fix: there is always a resource where this code is used. And if not, resourceRouter has validation that handles this.

--- test/unit/urls/urlParser.test.ts
+++ test/unit/urls/urlParser.test.ts
@@ -1,7 +1,6 @@
 import { test, expect } from 'vitest'
 import { parseUrl } from '../../../src/urls/urlParser'
 
-const noResourceAndNoId = { resource: null, id: null }
 const resourceOnly = { resource: 'stuff', id: null }
 const resourceAndId = { resource: 'stuff', id: 'foo' }
 
@@ -34,14 +33,6 @@ test.each([
   ['stuff/foo/bar/', resourceAndId],
   ['/stuff/foo/bar/', resourceAndId],
 ])("URL '%s' has additional path items next to a resource and id: %o", (url, expected) => {
-  const actual = parseUrl(url)
-  expect(actual).toEqual(expected)
-})
-
-test.each([
-  ['', noResourceAndNoId],
-  [' ', noResourceAndNoId],
-])("URL '%s' has no resource and id: %o", (url, expected) => {
   const actual = parseUrl(url)
   expect(actual).toEqual(expected)
 })
