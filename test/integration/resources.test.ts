3f6582222e3b7486d99b68565560567f7da0ab38
Bouwe <bouwe@webinargeek.com>
2024-09-06 14:01:11 +0200

    feat: support both resources as strings and as extended objects

--- test/integration/resources.test.ts
+++ test/integration/resources.test.ts
@@ -8,12 +8,23 @@ import createServer from './createServer'
 
 test('Only configured resources can be found, others return a 404', async () => {
   const tembaServer = createServer({
-    resources: ['movies'],
+    resources: [
+      'movies',
+      {
+        resourcePath: 'people',
+        singularName: 'person',
+        pluralName: 'people',
+      },
+    ],
   })
 
   // The movies resource is configured, so it can be found.
-  const successResponse = await request(tembaServer).get('/movies')
-  expect(successResponse.statusCode).toEqual(200)
+  const moviesResponse = await request(tembaServer).get('/movies')
+  expect(moviesResponse.statusCode).toEqual(200)
+
+  // People are configured as an extended resource, so it can be found.
+  const peopleResponse = await request(tembaServer).get('/people')
+  expect(peopleResponse.statusCode).toEqual(200)
 
   // The actors resource is not configured, so it can not be found.
   const response = await request(tembaServer).get('/actors')
