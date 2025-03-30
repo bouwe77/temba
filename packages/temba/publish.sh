0db159dc082dd698b3e9030adc8b85c7f90ab620
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2025-03-30 16:35:08 +0200

    feat: update CLI and examples version of the Temba dependency

--- packages/temba/publish.sh
+++ packages/temba/publish.sh
@@ -14,18 +14,22 @@ then
 fi
 
 if [ -z "$(git status --porcelain)" ]; then 
+    # Publish Temba to npm
     version=$(npm version $1)
     npm run build
     echo "export const version = '$version'\n" > ./dist/src/version.js
     npm publish ./dist/src
 
+    # Publish CLI to npm, and update Temba version in starter-template
     cd ../cli
-    node update-version.js $version
+    node ../../update-version.js $version create/starter-template
     npm version $version
     npm publish
 
-    cd ../temba
+    # Update Temba version in examples
+    node ../../update-version.js $version examples
 
+    cd ../temba
     open "https://github.com/bouwe77/temba/releases/new?tag=$version&title=$version&prerelease=1"
 else 
   echo "Error: Commit all changes before publishing"
