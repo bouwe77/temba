2b0091dc2ea5b7f7142d8113a1f1b7edba616f38
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2025-05-25 15:09:43 +0200

    tests: neq operator

--- packages/temba/test/integration/filtering.test.ts
+++ packages/temba/test/integration/filtering.test.ts
@@ -39,6 +39,7 @@ test('GET - Filter using [eq] operator', async () => {
   const queryStrings = [
     'filter.name[eq]=Miep',
     'filter.name[eq]=miep',
+    // No operator defaults to [eq]
     'filter.name=Miep',
     'filter.name=MIEP',
   ]
@@ -48,5 +49,31 @@ test('GET - Filter using [eq] operator', async () => {
     expectSuccess(getMiepResponse)
     expect(getMiepResponse.body.length).toEqual(1)
     expect(getMiepResponse.body[0].name).toEqual('Miep')
+  }
+})
+
+test('GET - Filter using [neq] operator', async () => {
+  const tembaServer = await createServer()
+
+  // Create 2 resources
+  const data = [{ name: 'Piet' }, { name: 'Miep' }]
+  await createData(tembaServer, data)
+
+  // Get all resources without filtering
+  const getAllResponse = await request(tembaServer).get(resource)
+  expectSuccess(getAllResponse)
+  expect(getAllResponse.body.length).toEqual(2)
+  expect(getAllResponse.body.map((item: { name: string }) => item.name)).toEqual(
+    data.map((item) => item.name),
+  )
+
+  // Filter case-insensitive using [neq] operator
+  const queryStrings = ['filter.name[neq]=Miep', 'filter.name[neq]=miep']
+
+  for (const queryString of queryStrings) {
+    const getMiepResponse = await request(tembaServer).get(resource).query(queryString)
+    expectSuccess(getMiepResponse)
+    expect(getMiepResponse.body.length).toEqual(1)
+    expect(getMiepResponse.body[0].name).toEqual('Piet')
   }
 })
