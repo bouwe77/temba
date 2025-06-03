9a42e9305ebbd4e5aa3e22e8e4e96ee5f90085f7
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2025-06-03 20:50:56 +0200

    docs(etags): clarify etag usage

--- packages/temba/src/etags/etags.ts
+++ packages/temba/src/etags/etags.ts
@@ -1,6 +1,6 @@
 import etagFromExpress, { type StatsLike } from 'etag'
 
-// Temba uses generating etags both for generating etags and for comparing etags.
+// Temba uses the `etag` module both to generate and compare ETags.
 // Express only supports generating them, so to be sure we use the same algorithm for both,
 // we use the etag module for both by wrapping it here.
 // This means we also override the default Express etag function, but that's with the same algorithm.
