f819f56ca48d17601a2c9212fdf6a2e17ec89b80
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2025-10-05 13:36:32 +0200

    add straight to the docs button

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,35 +1,35 @@
-import React from "react";
-import clsx from "clsx";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-import styles from "./index.module.css";
+import React from 'react'
+import clsx from 'clsx'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import HomepageFeatures from '@site/src/components/HomepageFeatures'
+import Heading from '@theme/Heading'
+import styles from './index.module.css'
+import Link from '@docusaurus/Link'
+import CodeBlock from '@theme/CodeBlock' // Docusaurus’s built-in code renderer
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Temba Tutorial - 29 secs ⏱️
+        <div className={styles.buttons}>
+          <Link className={`button button--lg ${styles.docsButton}`} to="/docs/documentation">
+            Straight to the docs 🚀
           </Link>
-        </div> */}
+        </div>
       </div>
     </header>
-  );
+  )
 }
-
-import CodeBlock from "@theme/CodeBlock"; // Docusaurus’s built-in code renderer
 
 function CodeSection() {
   return (
-    <section className={clsx("container margin-vert--xl", styles.codeSection)}>
+    <section className={clsx('container margin-vert--xl', styles.codeSection)}>
       <h1 className="text--center">Do you haz teh codez?</h1>
       <b>1. Installation</b>
       <CodeBlock className={styles.codeBlock} language="bash">
@@ -50,11 +50,11 @@ server.start()`}
         {`npx temba-cli create my-api`}
       </CodeBlock>
     </section>
-  );
+  )
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout title={`${siteConfig.title}`} description={siteConfig.tagline}>
       <HomepageHeader />
@@ -63,5 +63,5 @@ export default function Home(): JSX.Element {
         <CodeSection />
       </main>
     </Layout>
-  );
+  )
 }
